e365387b1b4d0e1e405e6c1dc001cbd2b8a13aa5
brainboxemb <285184499+brainboxemb@users.noreply.github.com>
2026-09-10 15:34:32 +0200

    Align tooling tests v0.9.0

--- tests/test_tooling.py
+++ tests/test_tooling.py
@@ -4,7 +4,7 @@ from scad_project.config import ProjectContext
 from scad_project.tooling import tooling_errors
 
 
-def context(ref="v0.8.0"):
+def context(ref="v0.9.0"):
     return ProjectContext(
         root=Path("."),
         config_file=Path("project.yml"),
